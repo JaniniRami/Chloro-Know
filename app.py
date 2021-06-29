dfe97b8df94e252e16d8a066db3adda215a50b9a
Rami Janini <>
2021-06-29 10:41:01 +0300

    Init

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ import altair as alt
 import matplotlib.pyplot as plt
 import plotly.figure_factory as ff
 import plotly.express as px
-from models.process_data import tokyoData, veniceData, newYorkData
-
+from models.process_data import tokyoData, veniceData, newYorkData, movingAverage
+import streamlit.components.v1 as components
 
 st.set_page_config(
-        page_title="Nimbus Soulution",
+        page_title="Chloro-kNOw Solution ",
         page_icon=":globe_with_meridians:",
         layout="wide")
 
@@ -22,12 +22,9 @@ st.set_page_config(
 matplotlib.use("agg")
 _lock = RendererAgg.lock
 
-
 sns.set_style('darkgrid')
 row0_spacer1, row0_1, row0_spacer2, row0_2, row0_spacer3 = st.beta_columns(
     (.1, 2, .2, 1, .1))
-
-
 
 st.cache(persist=True)
 def load_data():
@@ -39,15 +36,13 @@ data_load_state = st.text('Loading data, please wait...')
 covid19 = load_data()
 data_load_state.text('')
 
-
 def main():
     row0_1.title('Looking At The Bigger Picture')
-
     with row0_2:
         st.write('')
 
     row0_2.subheader(
-        'Soulution by the [Nimbus Team](https://github.com/bykevinyang/Nimbus)')
+        'Solution by the [Chloro-kNOw Team](https://github.com/bykevinyang/Chloro-kNOw)')
 
     row1_spacer1, row1_1, row1_spacer2 = st.beta_columns((.1, 3.2, .1))
 
@@ -58,9 +53,7 @@ def main():
     st.write('')
     row3_space1, row3_1, row3_space2, row3_2, row3_space3 = st.beta_columns(
         (.1, 1, .1, 1, .1))
-
-
-    # TOKYO COMPONENT
+    # country = st.multiselect('Select a country', ['Japan', 'Italy', 'United States'])
     with row3_1, _lock:
         st.header("Tokyo Data 🇯🇵 ")
 
@@ -68,155 +61,116 @@ def main():
         @st.cache(allow_output_mutation=True)
         def load_data():
             return tokyoData()
+
+
         data_load_state = st.text('Loading Tokyo data...')
         chl_df_tokyo = load_data()[0]
-        tsm_df_tokyo = load_data()[1]
+        air_df_tokyo = load_data()[1]
+        activity_df_japan = load_data()[2]
         data_load_state.text('')
 
 
-        st.dataframe(load_data()[2])
-        # year_to_filter = st.slider('Filter years', 2019, 2021, 2021, key='tokyo')  # min: 0h, max: 23h, default: 17h
+        st.dataframe(load_data()[-1])
         st.markdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
 
 
-
     with row3_2, _lock:
-        st.header("Plot of Tokyo's Data")
+        st.header("Chlorophyll-a concentration in Japan")
         chl_df_tokyo.sort_values(by='Time', ascending=True, inplace=True)
-        chl_df_tokyo['time'] = pd.to_datetime(chl_df_tokyo.Time)
-        # chl_df_tokyo = chl_df_tokyo[chl_df_tokyo['time'].dt.year == year_to_filter]
 
+        chl_df_tokyo['time'] = pd.to_datetime(chl_df_tokyo.Time)
         fig = Figure()
         ax = fig.subplots()
-        # plt.figure(figsize=(15, 15))
-
+        plt.figure(figsize=(15, 15))
+        smoothed = movingAverage()
         ax.plot(chl_df_tokyo['Time'], chl_df_tokyo['Measurement Value'], color='darkorange',  label='Chl Concentration')
+        # ax.plot(smoothed['Time'], smoothed['Smoothed'], color='darkorange',  label='Chl Concentration')
+        # ax.plot(covid19[covid19["location"]=='Japan']['date'], covid19[covid19["location"]=='Japan']['new_cases']/20/2, color='green',  label='Covid cases')
         ax.legend()
         ax.tick_params(labelsize=7)
         ax.set_xlabel('Time (YY/MM)', fontsize=10)
         ax.set_ylabel('Measurement value (molarity)', fontsize=10)
         st.pyplot(fig)
-
-
 
     st.write('')
     row4_space1, row4_1, row4_space2, row4_2, row4_space3 = st.beta_columns(
         (.1, 1, .1, 1, .1))
 
+    with row4_2, _lock:
+        st.header("Interesting dates for Japan")
+        st.markdown('''The concentration of Chlorophyll-a is tested in lakes to determine how much algae is in the lake. Our solution shows the relationship between human activities and algae bloom, through out the COVID-19 pandemic human activities became decreasing and in result the concentration of Chl-a began decreasing to understand the dates below you must understand this first:
+        \n Any decrease of the concentration of CHL was caused because of the high reported COVID-19 cases which meant less human activity in the past two months of the CHL decrease date.
+        \n On the other hand, any increase of the concentration of CHL was caused because of the decrease of the reported COVID-19 cases which meant more human activity in the past two months of the CHL increase date.
+        ''')
+        expander_1 = st.beta_expander("The largest drop of concentration reported at 2020-08-22 in Japan.")
+        expander_1.write('The largest drop of concentration was reported in 2020-08-22 with a decrease value of -87.3 in Tokyo, and if we look at the month before the date 2020-08-22 we can see the increase of COVID cases in Japan during the month of June and July of 2020:')
+        button_1 = expander_1.button('Visualize', key='3')
+        button_1_end = expander_1.button('Stop Visualizing', key='3')
+
+
+        expander_2 = st.beta_expander("The largest increase of concentration reported at 2019-11-09 in Japan.")
+        expander_2.write('''The largest increase in the CHL concentration was reported in 2019-11-09 with an increase value of +265.03 in Tokyo, and if we take a look at the months before June we can realize that the increase happened before the Pandemic. ''')
+
     with row4_1, _lock:
         country = 'Japan'
         st.header("Japan Covid Cases")
-        fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases')
-        fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True, legend=dict(x=-.5, y=-2))
-        fig.update_yaxes(title_text='New Cases')
-        fig.update_xaxes(title_text='Date')
-        st.plotly_chart(fig)
+        if button_1:
+            japan_covid_fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases', color='tokyo_increase_color')
+            japan_covid_fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True)
+            japan_covid_fig.update_yaxes(title_text='New Cases')
+            japan_covid_fig.update_xaxes(title_text='Date')
+            st.plotly_chart(japan_covid_fig)
+        elif button_1_end:
+            japan_covid_fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases')
+            japan_covid_fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True)
+            japan_covid_fig.update_yaxes(title_text='New Cases')
+            japan_covid_fig.update_xaxes(title_text='Date')
+            st.plotly_chart(japan_covid_fig)
+        else:
+            japan_covid_fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases')
+            japan_covid_fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True)
+            japan_covid_fig.update_yaxes(title_text='New Cases')
+            japan_covid_fig.update_xaxes(title_text='Date')
+            st.plotly_chart(japan_covid_fig)
 
-    with row4_2, _lock:
-        st.header("Cool dates")
-
-    #ITALY
     st.write('')
-    st.markdown("""---""")
     row5_space1, row5_1, row5_space2, row5_2, row5_space3 = st.beta_columns(
         (.1, 1, .1, 1, .1))
 
     with row5_1, _lock:
-        st.header("Venice Data 🇮🇹")
-        @st.cache(allow_output_mutation=True)
-        def load_data():
-            return veniceData()
-        data_load_state = st.text('Loading Venice data...')
-        chl_df_venice = load_data()[0]
-        tsm_df_venice = load_data()[1]
-        data_load_state.text('')
-        st.dataframe(load_data()[2])
-        # year_to_filter = st.slider('Filter years', 2019, 2021, 2021, key='venice')  # min: 0h, max: 23h, default: 17h
-        st.markdown("Note that the publication date on Goodreads is the **last** publication date, so Chl Concentration is altered for any book that has been republished by a publisher.")
-
+        st.header("Chlorophyll-a concentration map in Japan")
+        components.html('<iframe class="item" src="https://eodashboard.org/iframe?poi=JP01-N3a2"  width="600px" height="500px" frameBorder="0" scroll="no" style="overflow:hidden"></iframe>', height=500, width=1000)
+        st.markdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
 
     with row5_2, _lock:
-        st.header("Plot of Venice's Data ")
-        chl_df_venice.sort_values(by='Time', ascending=True, inplace=True)
-        chl_df_venice['time'] = pd.to_datetime(chl_df_venice.Time)
-        # chl_df_venice = chl_df_venice[chl_df_tokyo['time'].dt.year == year_to_filter]
-        fig = Figure()
-        ax = fig.subplots()
-        ax.plot(chl_df_venice['Time'], chl_df_venice['Measurement Value'], color='darkorange',  label='Chl Concentration')
-        ax.legend()
-        ax.tick_params(labelsize=7)
-        ax.set_xlabel('Time (YY/MM)', fontsize=10)
-        ax.set_ylabel('Measurement value (molarity)', fontsize=10)
-        st.pyplot(fig)
+        st.header("Recovery Proxy map in Japan")
+        components.html('<iframe class="item" src="https://eodashboard.org/iframe?poi=JP01-N8" width="600px" height="500px" frameBorder="0" scroll="no" style="overflow:hidden"></iframe>', height=500,width=1000)
+        st.markdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+
 
     st.write('')
     row6_space1, row6_1, row6_space2, row6_2, row6_space3 = st.beta_columns(
         (.1, 1, .1, 1, .1))
 
     with row6_1, _lock:
-        country = 'Italy'
-        st.header("Italy Covid Cases")
-        fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases')
-        fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True, legend=dict(x=-.5, y=-2))
-        fig.update_yaxes(title_text='New Cases')
-        fig.update_xaxes(title_text='Date')
-        st.plotly_chart(fig)
+        st.header("Air quality measurement in Japan")
+        japan_air_fig=px.bar(air_df_tokyo,x='time',y='measurement')
+        japan_air_fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True)
+        japan_air_fig.update_yaxes(title_text='Air Quality Index')
+        japan_air_fig.update_xaxes(title_text='Date')
+        st.plotly_chart(japan_air_fig)
+        st.markdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
 
     with row6_2, _lock:
-        st.header("Cool dates")
-
-    #UNITED STATES
-    st.write('')
-    st.markdown("""---""")
-    row7_space1, row7_1, row7_space2, row7_2, row7_space3 = st.beta_columns(
-        (.1, 1, .1, 1, .1))
-
-    with row7_1, _lock:
-        st.header("New York Data 🇺🇸 ")
-        @st.cache(allow_output_mutation=True)
-        def load_data():
-            return newYorkData()
-        data_load_state = st.text('Loading New York data...')
-        chl_df_newyork = load_data()[0]
-        tsm_df_newyork = load_data()[1]
-        data_load_state.text('')
-        st.dataframe(load_data()[2])
-        # year_to_filter = st.slider('Filter years', 2019, 2021, 2021, key='new-york')  # min: 0h, max: 23h, default: 17h
-        st.markdown("Note that the publication date on Goodreads is the **last** publication date, so Chl Concentration is altered for any book that has been republished by a publisher.")
+        st.header("Cars/Containers Acttivity in Japan")
+        japan_activity_fig = px.bar(activity_df_japan,x='time',y='measurement', color='colorCode')
+        japan_activity_fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True)
+        japan_activity_fig.update_yaxes(title_text='Car Density')
+        japan_activity_fig.update_xaxes(title_text='Date')
+        st.plotly_chart(japan_activity_fig)
+        st.markdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
 
 
-
-    with row7_2, _lock:
-        st.header("Plot of New-York's Data")
-        chl_df_newyork.sort_values(by='Time', ascending=True, inplace=True)
-        chl_df_newyork['time'] = pd.to_datetime(chl_df_newyork.Time)
-        # chl_df_newyork = chl_df_newyork[chl_df_tokyo['time'].dt.year == year_to_filter]
-
-        fig = Figure()
-        ax = fig.subplots()
-        # plt.figure(figsize=(15, 15))
-        ax.plot(chl_df_newyork['Time'], chl_df_newyork['Measurement Value'], color='darkorange',  label='Chl Concentration')
-        ax.legend()
-        ax.tick_params(labelsize=7)
-        ax.set_xlabel('Time (YY/MM)', fontsize=10)
-        ax.set_ylabel('Measurement value (molarity)', fontsize=10)
-        st.pyplot(fig)
-
-    st.write('')
-    row8_space1, row8_1, row8_space2, row8_2, row8_space3 = st.beta_columns(
-        (.1, 1, .1, 1, .1))
-
-    with row8_1, _lock:
-        country = 'United States'
-        st.header("United States Covid Cases")
-        fig=px.bar(covid19[covid19["location"]==country],x='date',y='new_cases')
-        fig.update_layout(title_x=0.5, xaxis_rangeslider_visible=True, legend=dict(x=-.5, y=-2))
-        fig.update_yaxes(title_text='New Cases')
-        fig.update_xaxes(title_text='Date')
-        st.plotly_chart(fig)
-
-    with row8_2, _lock:
-        st.header("Cool dates")
 
 if __name__ == '__main__':
     main()
--- models/process_data.py
+++ models/process_data.py
@@ -1,9 +1,11 @@
 import pandas as pd
-import streamlit as st
+# import streamlit as st
+from matplotlib import pyplot as plt
 
 
 def cleanChlData():
-    df = pd.read_csv('data/chl.csv')
+
+    df = pd.read_csv('data\chl.csv')
     df["Time"] = pd.to_datetime(df['Time'])
     geometry = [list(map(float, x.split(','))) for x in df.AOI]
     lat = pd.Series([x[0] for x in geometry])
@@ -19,6 +21,22 @@ def cleanChlData():
     return df
 
 
+def cleanAirQualityData():
+    df = pd.read_csv('data/tokyo_air_quality.csv')
+    df["time"] = pd.to_datetime(df['time'])
+    df['measurement']=pd.to_numeric(df['measurement'])
+    df = df[df['measurement'].notna()]
+    df = df[['time', 'measurement', 'eoSensor', 'inputData', 'colorCode']]
+    return df
+
+def cleanActivityData():
+    japan_ac = pd.read_csv('data/japan_activity.csv')
+    japan_ac["time"] = pd.to_datetime(japan_ac['time'])
+    japan_ac['measurement']=pd.to_numeric(japan_ac['measurement'])
+    japan_ac = japan_ac[['time', 'measurement', 'colorCode']]
+    japan_ac = japan_ac.reset_index(drop=True)
+    return japan_ac
+
 def veniceData():
     df = cleanChlData()
     venice_df = df.loc[df['Country'] == "IT"]
@@ -33,7 +51,12 @@ def tokyoData():
     tokyo_chl = tokyo_df.loc[tokyo_df['City'] == "Tokyo, Chl-a"]
     tokyo_tsm = tokyo_df.loc[tokyo_df['City'] == "Tokyo, TSM"]
     tokyo_df = tokyo_df.reset_index(drop=True)
-    return tokyo_chl, tokyo_tsm, tokyo_df
+
+    tokyo_air = cleanAirQualityData()
+    tokyo_air = tokyo_air.reset_index(drop=True)
+
+    japan_activity = cleanActivityData()
+    return tokyo_chl, tokyo_air, japan_activity, tokyo_df
 
 def newYorkData():
     df = cleanChlData()
@@ -42,3 +65,45 @@ def newYorkData():
     newyork_tsm = newyork_df.loc[newyork_df['City'] == "New York, TSM"]
     newyork_df = newyork_df.reset_index(drop=True)
     return newyork_chl, newyork_tsm, newyork_df
+
+def movingAverage():
+    LAGTIME = 5 # In months
+
+    output = []
+
+    df_venice = veniceData()[0]
+    df_newYork = newYorkData()[0]
+    df_tokyo = tokyoData()[0]
+
+    mapping = {
+        "Venice" : df_venice,
+        "New York" : df_newYork,
+        "Tokyo" : df_tokyo
+        }
+
+    cities = [("Venice", df_venice['Measurement Value']), ("New York", df_newYork['Measurement Value']), ("Tokyo", df_tokyo['Measurement Value'])]
+
+    for city in cities:
+        measurements = city[1].tolist()
+        ema = calculate_ema(measurements, LAGTIME, 4)
+        df = mapping[city[0]]
+        df["Smoothed"] = ema
+        output.append(("Venice", df))
+
+
+    # Ouputs tuple in the form of [(City Name, Data Frame),(City Name, Data Frame),(City Name, Data Frame)]
+    return df
+
+
+    # for p in smoothedValues:
+    #     plt.plot(p[1], label=p[0])
+
+    # plt.show()
+
+def calculate_ema(data, LAGTIME, smoothing=2):
+    ema = [sum(data[:LAGTIME]) / LAGTIME]
+
+    for mes in data[1:]:
+        ema.append((mes * (smoothing / (1 + LAGTIME))) + ema[-1] * (1 - (smoothing / (1 + LAGTIME))))
+
+    return ema
